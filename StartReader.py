745ceb2039eae197c48850c5ef08102bcd4229f6
Ash <ash@microweber.com>
2020-10-21 20:36:47 +0300

    Status reporting

--- StartReader.py
+++ StartReader.py
@@ -3,6 +3,8 @@
 import requests
 import base64
 import time
+import platform
+import psutil
 from uuid import getnode as get_mac
 
 url = 'http://raffy-admin/iot/start-reader'
@@ -11,6 +13,11 @@ reader = str(get_mac())
 reader_bytes = reader.encode('ascii')
 reader_base64 = base64.b64encode(reader_bytes)
 
-postdata = {'reader': reader_base64}
+postdata = {
+  'reader': reader_base64,
+  'platform': platform.platform(),
+  'cpu': psutil.cpu_percent(),
+  'ram': psutil.virtual_memory().percent
+}
 x = requests.post(url, data = postdata, verify = False)
 print(x.text)
--- start-reader.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo python3 StartReader.py > /home/pi/logs/start.log;
